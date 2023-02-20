c3ebb4df03804a740c82ddc17bea861dd1fb8fd4
SkyRain1225 <rudwns9286@gmail.com>
2023-02-21 07:43:58 +0900

    ✨ 가위바위보 이미지 추가

---
+++ src/assets/index.ts
@@ -0,0 +1,3 @@
+export { default as Rock } from './images/rock.png';
+export { default as Paper } from './images/paper.png';
+export { default as Scissors } from './images/scissors.png';
--- src/components/HelloWorld/HelloWorld.tsx
+++ src/components/HelloWorld/HelloWorld.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import { Paper, Rock, Scissors } from '~/assets';
 
 import * as S from './HelloWorld.styled';
 
 const HelloWorld = () => {
-  return <S.Container>Hi there</S.Container>;
+  return (
+    <S.Container>
+      가위 바위 보
+      <img src={Rock} alt="rock-image" />
+      <img src={Paper} alt="paper-image" />
+      <img src={Scissors} alt="scissors-image" />
+    </S.Container>
+  );
 };
 
 export default HelloWorld;
